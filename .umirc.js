b97e6ab8615949449a7119140d2b1df4a52d1409
yinfxs <yinfxs@gmail.com>
2019-06-26 17:54:21 +0800

    feat: 初始化项目

---
+++ .umirc.js
@@ -0,0 +1,30 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'HELLO REACT',
+      dll: false,
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+  proxy: {
+    '/api': {
+      target: 'https://k11-central.hofo.co',
+      changeOrigin: true
+    },
+  }
+}
